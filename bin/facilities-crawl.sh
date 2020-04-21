5aa7b70689528d56d0a73a2eb130c94482e502aa
Bryan Schofield <bryan.schofield@libertyits.com>
2020-04-21 14:46:42 -0500

    better menu for jk master, facilities-crawl now does zip, state, and lat+long queries

--- bin/facilities-crawl.sh
+++ bin/facilities-crawl.sh
@@ -11,17 +11,22 @@ onExit() { rm $OUT; }
 trap onExit EXIT
 
 request() {
-  local path=$1
-  curl -sHapikey:${API_KEY} -k $LB/$FACILITIES_PATH/$path -o $OUT -w "$(date +'%D %T') %{http_code} %{time_total} $path\n"
+  local path="$1"
+  curl -sHapikey:${API_KEY} -k "$LB/$FACILITIES_PATH/$path" -o $OUT -w "$(date +'%D %T') %{http_code} %{time_total} $path\n"
 }
 
 request facilities/all
 ids=( $(jq -r '.features[].properties.id' $OUT) )
 for id in "${ids[@]}"
-do 
+do
   request facilities/$id
   lat="$(jq -r '.data.attributes.lat' $OUT)"
-  long="$(jq -r '.data.attributes.long' $OUT)" 
-   request "nearby?lat=$lat&lng=$long"
+  long="$(jq -r '.data.attributes.long' $OUT)"
+  zip="$(jq -r '.data.attributes.address.physical.zip' $OUT)"
+  state="$(jq -r '.data.attributes.address.physical.state' $OUT)"
+  request "facilities?zip=${zip%-*}"
+  request "facilities?state=$state"
+  request "facilities?lat=$lat&lng=$long&ids=$id"
+  request "nearby?lat=$lat&lng=$long"
 done
 
